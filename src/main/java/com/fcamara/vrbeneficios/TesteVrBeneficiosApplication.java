da4646a88d1e91dc07a36845d2509d992e2b3452
Andre santini <andresantini123@gmail.com>
2024-10-17 13:16:58 -0300

    init project for test

---
+++ src/main/java/com/fcamara/vrbeneficios/TesteVrBeneficiosApplication.java
@@ -0,0 +1,13 @@
+package com.fcamara.vrbeneficios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteVrBeneficiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteVrBeneficiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcamara/vrbeneficios/TesteVrBeneficiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcamara.vrbeneficios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteVrBeneficiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
